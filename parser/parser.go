ee9385a8d8b0bf5dbfb904636749324de6f325c5
gnocarroll <gocarrol@purdue.edu>
2025-09-12 14:36:06 -0500

    initial work on parsing

--- parser/parser.go
+++ parser/parser.go
@@ -4,13 +4,36 @@ import (
 	"pegasus/scanner"
 )
 
-type Parser struct {
+const MAX_BUFFERED_NODES = 1000000
+
+func (parser *Parser) initParser() {
+	nodeChan := make(chan INode, MAX_BUFFERED_NODES)
+
+	parser.nodeChan = &nodeChan
+}
+
+func NewParser() Parser {
+	var parser Parser
+
+	parser.initParser()
+
+	return parser
 }
 
 func (parser *Parser) parse(scan *scanner.Scanner) {
+	f := parser.parseFile(scan)
 
+	*parser.nodeChan <- f
 }
 
 func (parser *Parser) Parse(scan *scanner.Scanner) {
+	if parser.nodeChan == nil {
+		parser.initParser()
+	}
+
 	go parser.parse(scan)
+}
+
+func (parser *Parser) parseFile(scan *scanner.Scanner) *File {
+	return nil
 }
--- parser/types.go
+++ parser/types.go
@@ -1,23 +1,5 @@
 package parser
 
-type File struct {
+type Parser struct {
+	nodeChan *chan INode
 }
-
-type Definition interface {
-	definitionTag()
-}
-
-type VarDefinition struct {
-}
-
-func (VarDefinition) definitionTag() {}
-
-type TypeDefinition struct {
-}
-
-func (TypeDefinition) definitionTag() {}
-
-type FunctionDefinition struct {
-}
-
-func (FunctionDefinition) definitionTag() {}
