9a20a2111ec1046e7bd2aabd68d2d6f023d8464a
gnocarroll <gocarrol@purdue.edu>
2025-09-14 19:56:00 -0400

    done for the day... probablyc

--- parser/literals.go
+++ parser/literals.go
@@ -1,15 +1,45 @@
 package parser
 
-import "pegasus/scanner"
+import (
+	"errors"
+	"log"
+	"pegasus/scanner"
+	"strconv"
+)
 
-func IntegerLiteralFromTok(tok *scanner.Token) {
+func IntegerLiteralFromTok(tok *scanner.Token) (*IntegerLiteral, error) {
+	if tok.TType != scanner.TOK_INTEGER {
+		return nil, errors.New("expected integer token")
+	}
+
+	value, err := strconv.ParseUint(
+		tok.Text,
+		0,
+		64,
+	)
+
+	if err != nil {
+		log.Printf("Expected integer in \"%s\"", tok.Text)
+		value = 0
+	}
+
+	ret := &IntegerLiteral{
+		Value: value,
+	}
+	ret.SetPosition(tok.Line, tok.Column)
+
+	return ret, nil
+}
+
+func FloatLiteralFromTok(tok *scanner.Token) (*FloatLiteral, error) {
+	if tok.TType != scanner.TOK_FLOAT {
+		return nil, errors.New("expected float token")
+	}
 
 }
 
-func FloatLiteralFromTok(tok *scanner.Token) {
-
-}
-
-func StringLiteralFromTok(tok *scanner.Token) {
-
+func StringLiteralFromTok(tok *scanner.Token) (*StringLiteral, error) {
+	if tok.TType != scanner.TOK_STRING {
+		return nil, errors.New("expected float token")
+	}
 }
